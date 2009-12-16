b929cf1d402b63b85184f9bc94d10ccddcb4cb36
Gustavo Picon <tabo@tabo.pe>
2009-12-16 11:35:14 -0500

    fixed path bug in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ classifiers = [
 ]
 
 root_dir = os.path.dirname(__file__)
-long_desc = open(root_dir if root_dir else '.'+'/README').read()
+long_desc = open((root_dir if root_dir else '.')+'/README').read()
 
 setup(
     name='numconv',
