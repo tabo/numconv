d517114e5973f5623f67c867a7bf1eb833f52fed
Gustavo Picon <tabo@tabo.pe>
2014-04-15 16:26:14 -0400

    Adding python 3.3 and 3.4 to the list of supported versions in setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,8 @@ setup_args = dict(
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries'])
 
